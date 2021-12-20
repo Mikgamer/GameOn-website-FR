7110fe93ed1f6ab1a7124bfd1f5642bfd81223bf
Mikgamer <50680361+Mikgamer@users.noreply.github.com>
2021-12-20 16:34:53 +0100

    Close #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,23 +10,14 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const modalClose = document.querySelector(".close"); // Button for closing the modal form
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// close modal event
-modalClose.addEventListener("click", closeModal);
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
-}
-
-// Close modal form
-function closeModal() {
-  modalbg.style.display = "none";
 }
 
 
